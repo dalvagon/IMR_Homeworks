a33fda1d1b4a0045cc15557670658f2c7ff0cf49
Paul513607 <79131709+Paul513607@users.noreply.github.com>
2022-10-03 17:18:27 +0300

    fixed issues with running

--- Assets/Scripts/SceneBehavior.cs
+++ Assets/Scripts/SceneBehavior.cs
@@ -22,28 +22,41 @@ public class SceneBehavior : MonoBehaviour
     void Awake() {
         cactusObj = GameObject.FindGameObjectsWithTag("Cactus")[0];
         angryLogObj = GameObject.FindGameObjectsWithTag("Log")[0];
+
+        cactusObj.transform.LookAt(angryLogObj.transform);
+        angryLogObj.transform.LookAt(cactusObj.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        cactusObj.transform.LookAt(angryLogObj.transform);
-        angryLogObj.transform.LookAt(cactusObj.transform);
-
         float distance = Vector3.Distance(cactusObj.transform.position, angryLogObj.transform.position);
 
         if (mAnimatorCactus != null && mAnimatorAngryLog != null) 
         {
             if (distance < EPSILON)
             {
+                // Start the attack and run animations
                 mAnimatorCactus.SetTrigger("CactusTrigger1");
                 mAnimatorAngryLog.SetTrigger("AngryLogTrigger1");
-                float movementSpeed = 1.0F;
-                angryLogObj.transform.position += angryLogObj.transform.forward * Time.deltaTime * movementSpeed;
+
+                // Make AngryLog turn around and run
+                angryLogObj.transform.Rotate(18 0, Time.deltaTime * 30, 0, Space.Self);
+
+                if (distance > (EPSILON / 4)) 
+                {
+                    float movementSpeed = 1.0F;
+                    angryLogObj.transform.position += angryLogObj.transform.forward * Time.deltaTime * movementSpeed;
+                }
             }
             else
             {
+                // Revert to the idle state
                 mAnimatorCactus.SetTrigger("CactusTrigger2");
+                angryLogObj.transform.LookAt(cactusObj.transform);
+
+                cactusObj.transform.LookAt(angryLogObj.transform);
+                angryLogObj.transform.LookAt(cactusObj.transform);
             }
         }
     }
