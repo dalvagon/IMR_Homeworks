ce1fbf1b426f2f6cbea66d5acc130aadd22cf435
Paul513607 <79131709+Paul513607@users.noreply.github.com>
2022-10-11 14:20:53 +0300

    scoring

--- Assets/Scripts/ButtonTrigger.cs
+++ Assets/Scripts/ButtonTrigger.cs
@@ -1,16 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ButtonTrigger : MonoBehaviour
 {
     private Renderer renderer;
     private Color oldColor;
 
+    public GameObject leftHand;
+    public GameObject rightHand;
+    public GameObject finishButton;
+
+    public GameObject canvas;
+    public GameObject scoreText;
+
+    TextMeshProUGUI textMesh_scoreText;
+    Vector3 leftHand_initialPosition;
+    Vector3 rightHand_initialPosition;
+
+    float SCORE_MULTIPLIER = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         renderer = GetComponent<Renderer>();
+        canvas.SetActive(false);
+    }
+
+    void Awake()
+    {
+        canvas = GameObject.FindGameObjectsWithTag("Canvas")[0];
+        scoreText = GameObject.FindGameObjectsWithTag("Score text")[0];
+        textMesh_scoreText = scoreText.GetComponent<TextMeshProUGUI>();
+
+        leftHand = GameObject.FindGameObjectsWithTag("LeftHand")[0];
+        leftHand_initialPosition = leftHand.transform.position;
+        rightHand = GameObject.FindGameObjectsWithTag("RightHand")[0];
+        rightHand_initialPosition = rightHand.transform.position;
+        finishButton = GameObject.FindGameObjectsWithTag("Finish")[0];
     }
 
     // Update is called once per frame
@@ -23,10 +52,25 @@ public class ButtonTrigger : MonoBehaviour
     {
         oldColor = renderer.material.color;
         renderer.material.color = Color.red;
+
+        // Calculate score
+        float distance = 0;
+        if (leftHand.transform.position != leftHand_initialPosition)
+        {
+            distance = Vector3.Distance(leftHand.transform.position, finishButton.transform.position);
+        } else if (rightHand.transform.position != rightHand_initialPosition)
+        {
+            distance = Vector3.Distance(rightHand.transform.position, finishButton.transform.position);
+        }
+        float score = distance * SCORE_MULTIPLIER;
+
+        textMesh_scoreText.text = "Score: " + score.ToString();
+        canvas.SetActive(true);
     }
 
     void OnTriggerExit(Collider other)
     {
         renderer.material.color = oldColor;
+        canvas.SetActive(false);
     }
 }
